8fae1c34af60fa319ed50610bd0abe6d046cfbd2
Debangshu Nath <thecrusader.25225@gmail.com>
2024-06-03 18:04:30 +0530

    adding nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FaCss3, FaHtml5, FaJs } from "react-icons/fa";
+import { FaBeer, FaCss3, FaHtml5, FaJs } from "react-icons/fa";
 function App() {
   const [html, setHtml] = useState("");
   const [css, setCss] = useState("");
@@ -17,53 +17,56 @@ function App() {
     return () => clearTimeout(time);
   }, [html, css, js]);
   return (
-    <div className="h-screen w-screen absolute overflow-x-hidden p-4 bg-slate-950">
-      <div className="upper">
-        <div className="container">
-          <div className=" header">
-            <FaHtml5 className=" mt-1" />
-            <p>HTML</p>
+    <>
+      <div className="h-full w-screen absolute overflow-x-hidden p-4 pt-16 bg-slate-950">
+        <div className="upper">
+          <div className="container">
+            <div className=" header">
+              <FaHtml5 className=" mt-1" />
+              <p>HTML</p>
+            </div>
+            <textarea
+              className=" editor"
+              value={html}
+              onChange={(e) => {
+                setHtml(e.target.value);
+              }}
+            ></textarea>
           </div>
-          <textarea
-            className=" editor"
-            value={html}
-            onChange={(e) => {
-              setHtml(e.target.value);
-            }}
-          ></textarea>
-        </div>
-        <div className="container ">
-          <div className="header">
-            <FaCss3 className="mt-1" />
-            <p>CSS</p>
+          <div className="container ">
+            <div className="header">
+              <FaCss3 className="mt-1" />
+              <p>CSS</p>
+            </div>
+            <textarea
+              className=" editor"
+              value={css}
+              onChange={(e) => {
+                setCss(e.target.value);
+              }}
+            ></textarea>
           </div>
-          <textarea
-            className=" editor"
-            value={css}
-            onChange={(e) => {
-              setCss(e.target.value);
-            }}
-          ></textarea>
-        </div>
-        <div className="container ">
-          <div className=" header">
-            <FaJs className="mt-1" />
-            <p>JS</p>
+          <div className="container ">
+            <div className=" header">
+              <FaJs className="mt-1" />
+              <p>JS</p>
+            </div>
+            <textarea
+              className=" editor"
+              value={js}
+              onChange={(e) => {
+                setJs(e.target.value);
+              }}
+            ></textarea>
           </div>
-          <textarea
-            className=" editor"
-            value={js}
-            onChange={(e) => {
-              setJs(e.target.value);
-            }}
-          ></textarea>
         </div>
-      </div>
 
-      <div className="display">
-        <iframe srcDoc={sol} title="output" className="w-full h-full"></iframe>
+        <div className="display">
+          <iframe srcDoc={sol} title="output"></iframe>
+        </div>
+        <div className="fixed block top-0 left-0 h-16 w-full bg-yellow-400"></div>
       </div>
-    </div>
+    </>
   );
 }
 
